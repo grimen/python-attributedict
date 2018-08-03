95f6d280acc485fa044238d456150be18b0587e6
Jonas Grimfelt <grimen@gmail.com>
2018-08-02 22:39:30 -0400

    bugfix: include `install_requires` in `setup.py` - to avoid install dependency issues

--- setup.py
+++ setup.py
@@ -1,18 +1,54 @@
 
+# =========================================
+#       IMPORTS
+# --------------------------------------
+
+import os
 import setuptools
 
 # DISABLED/BUG: this line fails when `pip install attributedict` but works `pip install .`
 # from attributedict import __version__
 
-setuptools.setup(
-    name = 'attributedict',
-    version = '0.1.7',
-    description = (
+# =========================================
+#       FUNCTIONS
+# --------------------------------------
+
+def get_readme():
+    root_path = os.path.abspath(os.path.dirname(__file__))
+    readme_file_path = os.path.join(root_path, 'README.md')
+
+    with open(readme_file_path) as file:
+        readme = file.read()
+
+    return readme
+
+def get_requirements():
+    root_path = os.path.abspath(os.path.dirname(__file__))
+    requirements_file_path = os.path.join(root_path, 'requirements.txt')
+
+    with open(requirements_file_path) as file:
+        requirements = [requirement.strip() for requirement in file.readlines()]
+        requirements = filter(lambda requirement: len(requirement), requirements)
+        requirements = list(requirements)
+
+    return requirements
+
+
+# =========================================
+#       MAIN
+# --------------------------------------
+
+readme = get_readme()
+requirements = get_requirements()
+packages = setuptools.find_packages()
+
+config = {
+    'name': 'attributedict',
+    'version': '0.1.7',
+    'description': (
         'A dictionary object with attributes support.'
     ),
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    keywords = [
+    'keywords': [
         'object',
         'dict',
         'dictionary',
@@ -27,30 +63,20 @@ setuptools.setup(
         'struct',
         'hashmap',
     ],
-    author = 'Jonas Grimfelt',
-    author_email = 'grimen@gmail.com',
-    url = 'https://github.com/grimen/python-attributedict',
-    download_url = 'https://github.com/grimen/python-attributedict',
-    project_urls = {
+    'author': 'Jonas Grimfelt',
+    'author_email': 'grimen@gmail.com',
+    'url': 'https://github.com/grimen/python-attributedict',
+    'download_url': 'https://github.com/grimen/python-attributedict',
+    'project_urls': {
         'repository': 'https://github.com/grimen/python-attributedict',
         'bugs': 'https://github.com/grimen/python-attributedict/issues',
     },
-    packages = setuptools.find_packages(),
-    package_dir = {
-        'attributedict': 'attributedict'
-    },
-    package_data = {
-        '': [
-            'MIT-LICENSE',
-            'README.md',
-        ],
-        'attributedict': [
-            '*.*',
-        ]
-    },
-    py_modules = ['attributedict'],
-    license = 'MIT',
-    classifiers = [
+    'license': 'MIT',
+
+    'long_description': readme,
+    'long_description_content_type': 'text/markdown',
+
+    'classifiers': [
         'Topic :: Software Development :: Libraries',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -64,5 +90,25 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    zip_safe = True,
-)
+
+    'packages': packages,
+    'package_dir': {
+        'attributedict': 'attributedict'
+    },
+    'package_data': {
+        '': [
+            'MIT-LICENSE',
+            'README.md',
+        ],
+        'attributedict': [
+            '*.*',
+        ]
+    },
+    'include_package_data': True,
+    'zip_safe': True,
+
+    'install_requires': requirements,
+    'setup_requires': ['setuptools_git >= 1.2'],
+}
+
+setuptools.setup(**config)
