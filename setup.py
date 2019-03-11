240dc5f4620ec6783e379b02f35f0f07f4707ed8
Jonas Grimfelt <grimen@gmail.com>
2019-03-11 14:21:24 -0400

    refactor: cleanup `setup.py` using `setupextras`

--- setup.py
+++ setup.py
@@ -5,71 +5,52 @@
 
 import os
 import setuptools
+import setupextras
 
 # DISABLED/BUG: this line fails when `pip install attributedict` but works `pip install .`
 # from attributedict import __version__
-
-# =========================================
-#       FUNCTIONS
-# --------------------------------------
-
-def get_readme():
-    root_path = os.path.abspath(os.path.dirname(__file__))
-    readme_file_path = os.path.join(root_path, 'README.md')
-
-    with open(readme_file_path) as file:
-        readme = file.read()
-
-    return readme
-
-def get_requirements():
-    root_path = os.path.abspath(os.path.dirname(__file__))
-    requirements_file_path = os.path.join(root_path, 'requirements.txt')
-
-    with open(requirements_file_path) as file:
-        requirements = [requirement.strip() for requirement in file.readlines()]
-        requirements = filter(lambda requirement: len(requirement), requirements)
-        requirements = list(requirements)
-
-    return requirements
 
 
 # =========================================
 #       MAIN
 # --------------------------------------
 
-readme = get_readme()
-requirements = get_requirements()
-packages = setuptools.find_packages()
+name = ''
+version = '0.3.0'
+description = 'A dictionary object with attributes support - for Python.'
+keywords = [
+    'object',
+    'dict',
+    'dictionary',
+    'collection',
+    'attributes',
+    'attribute',
+    'attr',
+    'properties',
+    'property',
+    'props',
+    'openstruct',
+    'struct',
+    'hashmap',
+]
+
+packages = setupextras.get_packages()
+data_files = setupextras.get_data_files(['*.*'], os.path.join(name, 'tests', '__fixtures__'))
+requirements = setupextras.get_requirements()
+readme = setupextras.get_readme()
 
 config = {
-    'name': 'attributedict',
-    'version': '0.3.0',
-    'description': (
-        'A dictionary object with attributes support.'
-    ),
-    'keywords': [
-        'object',
-        'dict',
-        'dictionary',
-        'collection',
-        'attributes',
-        'attribute',
-        'attr',
-        'properties',
-        'property',
-        'props',
-        'openstruct',
-        'struct',
-        'hashmap',
-    ],
+    'name': name,
+    'version': version,
+    'description': (description),
+    'keywords': keywords,
     'author': 'Jonas Grimfelt',
     'author_email': 'grimen@gmail.com',
-    'url': 'https://github.com/grimen/python-attributedict',
-    'download_url': 'https://github.com/grimen/python-attributedict',
+    'url': 'https://github.com/grimen/python-{name}'.format(name = name),
+    'download_url': 'https://github.com/grimen/python-{name}'.format(name = name),
     'project_urls': {
-        'repository': 'https://github.com/grimen/python-attributedict',
-        'bugs': 'https://github.com/grimen/python-attributedict/issues',
+        'repository': 'https://github.com/grimen/python-{name}'.format(name = name),
+        'bugs': 'https://github.com/grimen/python-{name}/issues'.format(name = name),
     },
     'license': 'MIT',
 
@@ -84,31 +65,32 @@ config = {
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
     'packages': packages,
     'package_dir': {
-        'attributedict': 'attributedict'
+        name: name,
     },
     'package_data': {
         '': [
             'MIT-LICENSE',
             'README.md',
         ],
-        'attributedict': [
+        name: [
             '*.*',
-        ]
+        ],
     },
+    'data_files': data_files,
     'include_package_data': True,
     'zip_safe': True,
 
     'install_requires': requirements,
-    'setup_requires': ['setuptools_git >= 1.2'],
+    'setup_requires': [
+        'setuptools_git >= 1.2',
+    ],
 }
 
 setuptools.setup(**config)
